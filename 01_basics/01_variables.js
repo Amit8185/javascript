5503b33bff6df913bceae47f115834c609a1b57f
Amit8185 <149670584+Amit8185@users.noreply.github.com>
2024-07-21 06:59:38 +0000

    commit

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,21 @@
+const accountId =1444553
+let accountEmail="Amit@google.com"
+var accountPassword = "12345"
+accountcity="Jaipur"
+let accountState;
+
+
+// accountId=2;
+accountEmail="Amar@gmail.com"
+accountPassword="254"
+accountcity="Thane"
+
+
+/*
+prefer not to use var because of isue in block space and functiinal scope
+
+*/
+
+// console.log(accountId);
+console.table([accountId,accountEmail,accountPassword,accountcity,accountState]);
+
---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,23 @@
+"use strict"; //treat all js code as newer version
+
+//alert(3+3)  //we are using nodejs,not browser
+
+let name ="Amit"
+let age=18
+let isLoggedIn =false
+
+
+
+console.log(typeof  undeefined) //undefined
+console.log(typeof  null) //object
+
+//number -> 2 to power 53
+//bigint
+//string ->" "
+//boolean -> true/False
+//null -> standalone value
+//undefined
+//symbol -> unique
+
+
+//object
