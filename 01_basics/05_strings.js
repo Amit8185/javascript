0d093202252aa04e891c95d5ab12d041e7949ede
Amit8185 <149670584+Amit8185@users.noreply.github.com>
2024-07-27 11:16:59 +0000

    strings

---
+++ 01_basics/05_strings.js
@@ -0,0 +1,44 @@
+const name = "Amit";
+const repoCount =50;
+
+console.log(name + repoCount);
+
+console.log(`my name is ${name } and i have ${repoCount} repositories`);
+
+
+
+const gameName =new String("hitesh-hc");
+
+// console.log(gameName[0]);
+// console.log(gameName.__proto__);
+// console.log(gameName.length);
+// console.log(gameName.toUpperCase());
+// console.log(gameName.charAt(2));
+// console.log(gameName.indexOf('t'));
+
+// console.log(gameName.substring(0,4))
+
+// console.log(gameName.slice(-8,4));
+
+// const newstring = "    HITESH    "
+
+// console.log(newstring);
+// console.log(newstring.trim());
+
+
+
+// const url = "https://hitesh.com/hitesh%20choudhary";
+
+// console.log(url.replace('%20','-'));
+
+// console.log(url.includes("hitesh"));
+
+
+
+
+const newstring = "This is Amit, Amit how are you?"
+const firstoccurence = newstring.indexOf("Amit");
+const secondoccurence = newstring.indexOf("Amit",firstoccurence+1)
+console.log(firstoccurence);
+console.log(secondoccurence)
+
