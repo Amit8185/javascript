68fb23fadf0798fd69260c2356156f5be83cb63f
Amit8185 <149670584+Amit8185@users.noreply.github.com>
2024-07-28 07:46:00 +0000

    nums and maths

---
+++ 01_basics/06_nums_and_maths.js
@@ -0,0 +1,46 @@
+const score =400
+
+const balance = new Number(100);
+// console.log(balance);
+
+// console.log(balance.toString().length);
+
+// console.log(typeof(balance));
+
+//console.log(balance.toFixed(2));
+
+const otherNumber = 23.4999;
+
+//console.log(otherNumber.toPrecision(2));
+
+const hundereds = 1000000;
+
+//console.log(hundereds.toLocaleString('en-IN'));
+
+
+
+// +++++++++++++++++++++++ MATH ++++++++++++++++++++++++++++++++
+
+
+// console.log(Math.abs(30));
+// console.log(Math.round(4.3));
+// console.log(Math.ceil(4.2));
+// console.log(Math.floor(4.8));
+// console.log(Math.min(4.2,4.8,6.0,9.1));
+// console.log(Math.max(4.2,4.8,6.0,9.1));
+
+// console.log(Math.random());
+
+// console.log(Math.random()*10);
+
+console.log(Math.floor(Math.random()*10+1));
+
+const min =10;
+const max =20;
+
+console.log(Math.floor(Math.random()* (max-min+1) + min));
+
+// console.log(Math.random());
+
+
+
